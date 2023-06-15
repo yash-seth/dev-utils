5ff1a67d7d1b51ed5417ec28632e823fd79ac07a
Yash Seth <yashseth2002@gmail.com>
2023-06-15 21:11:49 +0530

    feat: add yt video embedder component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState } from "react"
 import SpeechRecognizer from './Components/SpeechRecognizer/SpeechRecognizer';
 import TTS from './Components/TTS/TTS';
+import VideoEmbedder from './Components/VideoEmbedder/VideoEmbedder';
 
 function App() {
   const [inputText, setInputText] = useState("")
@@ -9,6 +10,7 @@ function App() {
     <>
       <SpeechRecognizer setInputText={setInputText}/>
       <TTS inputText={inputText}/>
+      <VideoEmbedder embedId="PJbIhBmSR7Y"/>
     </>
   );
 }
